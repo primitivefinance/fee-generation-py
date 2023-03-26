194bf461a61114cd993d284a499c32bb8e03d2e8
Estelle Sterrett <83523407+0xEstelle@users.noreply.github.com>
2023-03-25 20:35:44 -0400

    Fixed GBM

--- Modules/Sim.py
+++ Modules/Sim.py
@@ -27,9 +27,9 @@ def simulate(env):
 
         def generateGBM(T, mu, sigma, p0, dt, env):
             N = round(T/dt)
-            t = T/dt*env.now
-            dW = np.random.standard_normal()
-            W = np.cumsum(dW)*np.sqrt(dt)
+            t = dt/T*env.now
+            dW = np.random.standard_normal(size=N)
+            W = np.cumsum(dW)[env.now]*np.sqrt(dt)
             P = p0*np.exp((mu - 0.5*sigma**2)*t + sigma*W)
             return P
 
