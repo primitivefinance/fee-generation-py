e6efd1f66bc2ab196913027c677a3badea31756d
Estelle Sterrett <estellesterrett@gmail.com>
2023-04-07 07:32:08 -0400

    Added basic multithreading

--- Modules/Sim.py
+++ Modules/Sim.py
@@ -1,48 +1,50 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from concurrent.futures import ThreadPoolExecutor
+import simpy
+
 from Arbitrage import referenceArbitrage as a
 from CFMM import RMM01
 from CFMM import StableVolatility
 from CFMM import ConstantSum
 import PriceGen as price
 from PriceGen import close_values
-import simpy
 
 # Simulation Choices
 
-run_GBM_simulation = True
-run_OU_simulation = False
-run_Backtest = True
+run_GBM_simulation = False
+run_OU_simulation = True
+run_Backtest = False
 run_ConstantSum_test = False
 
 # Config Params
 
-G = 20      # Number of Pool Realized Volatility Values
+G = 100      # Number of Pool Realized Volatility Values
 
-K1 = 1500                           # Strike of RMM-01 Pool
-p0 = 1500                           # Initial RMM01 and GBM Price
-K2 = 1                              # Strike of StableVolatility Pool
-P0 = 1                              # Initial OU and StableVolatility Price
-sigma = np.linspace(0.01, 0.4, G)   # Pool Implied Volatility Parameter
-T = 0.1                             # Pool Time Horizon in Years
-dt = 0.015/365                      # Time-Step Size in Years
-N = round(T/dt)                     # Number of Time-Steps
-gamma = 0.9999                      # Fee Regime on CFMM
-shares = 100000                     # Number of Shares in CFMM
+K1 = 1500                               # Strike of RMM-01 Pool
+p0 = 1500                               # Initial RMM01 and GBM Price
+K2 = 1                                  # Strike of StableVolatility Pool
+P0 = 1                                  # Initial OU and StableVolatility Price
+sigma = np.linspace(0.0001, 0.1, G)     # Pool Implied Volatility Parameter
+T = 0.1                                 # Pool Time Horizon in Years
+dt = 0.015/365                          # Time-Step Size in Years
+N = round(T/dt)                         # Number of Time-Steps
+gamma = 0.9999                          # Fee Regime on CFMM
+shares = 100000                         # Number of Shares in CFMM
 
-v = 0.1             # GBM Volatility Parameter
+v = 0.1             # GBM & OU Volatility Parameter
 mu = 0.0            # GBM Drift Parameter
 
 mean = 1            # OU mean price
 theta = 2/365       # OU mean reversion time
 
-Arb = 1             # Arbitrage Profit Threshold Denominated in Numeraire
+Arb = 5             # Arbitrage Profit Threshold Denominated in Numeraire
 
-M = 1              # Number of Simulation Runs per IV parameter
+M = 1               # Number of Simulation Runs per IV parameter
 
 # Simulation Processes
 
-def simulateGBM(env, i):
+def simulateGBM(env, i, Fees):
     CFMM = RMM01(p0, K1, sigma[i], T, dt, gamma, env, shares)
     
     while True:
@@ -53,7 +55,7 @@ def simulateGBM(env, i):
         Fees.append(arb.Fees)
         yield env.timeout(1)
 
-def simulateOU(env, i):
+def simulateOU(env, i, Fees):
     CFMM = StableVolatility(P0, K2, sigma[i], T, gamma, env, shares)
 
     while True:
@@ -64,7 +66,7 @@ def simulateOU(env, i):
         Fees.append(arb.Fees)
         yield env.timeout(1)
 
-def simulateBacktest(env, i):
+def simulateBacktest(env, i, Fees):
     CFMM = StableVolatility(P0, K2, sigma[i], T, gamma, env, shares)
 
     while True:
@@ -86,47 +88,62 @@ def simulateConstantSum(env):
 
 # GBM Based RMM01 simulation
 
+def GBMSimProcess(j):
+    FeeIncome = []
+    for i in range (0, M):
+        Fees = []
+        env = simpy.Environment()
+        env.process(simulateGBM(env, j, Fees))
+        env.run(until=N)
+
+        FeeIncome.append(sum(Fees))
+    return sum(FeeIncome)/M
+
+## Multithreading
+
 array = []
 if run_GBM_simulation:
-    for j in range (0, G):     
-        FeeIncome = []        
-        for i in range (0, M):
-            Fees = []
-            env = simpy.Environment()
-            env.process(simulateGBM(env, j))
-            env.run(until=N)
-        
-            FeeIncome.append(sum(Fees))
-        array.append(sum(FeeIncome)/M)
+    with ThreadPoolExecutor() as executor:
+        array = list(executor.map(GBMSimProcess, range(G)))
 
 # OU Based Stable Volatility simulation
 
+def OUSimProcess(j):
+    FeeIncome = []
+    for i in range (0, M):
+        Fees = []
+        env = simpy.Environment()
+        env.process(simulateOU(env, j, Fees))
+        env.run(until=N)
+
+        FeeIncome.append(sum(Fees))
+    return sum(FeeIncome)/M
+
+## Multithreading
+
 array2 = []
 if run_OU_simulation:
-    for j in range (0, G):
-        FeeIncome = []
-        for i in range (0, M):
-            Fees = []
-            env = simpy.Environment()
-            env.process(simulateOU(env, j))
-            env.run(until=N)
-
-            FeeIncome.append(sum(Fees))
-        array2.append(sum(FeeIncome)/M)
+    with ThreadPoolExecutor() as executor:
+        array2 = list(executor.map(OUSimProcess, range(G)))
 
 # Backtest Stable Volatility simulation
 
+def BacktestProcess(j):
+    FeeIncome = []
+    Fees = []
+    env = simpy.Environment()
+    env.process(simulateBacktest(env, j, Fees))
+    env.run(until=len(price.close_values))
+
+    FeeIncome.append(sum(Fees))
+    return sum(FeeIncome)
+
+## Multithreading
+
 array3 = []
 if run_Backtest:
-    for j in range (0, G):
-        FeeIncome = []
-        Fees = []
-        env = simpy.Environment()
-        env.process(simulateBacktest(env, j))
-        env.run(until=len(price.close_values))
-
-        FeeIncome.append(sum(Fees))
-        array3.append(sum(FeeIncome))
+    with ThreadPoolExecutor() as executor:
+        array3 = list(executor.map(BacktestProcess, range(G)))
 
 # Constant Sum OU Test
 
