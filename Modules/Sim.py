fbdcd98d3682b022aa9949a0a88f94f56f483a1c
Estelle Sterrett <estellesterrett@gmail.com>
2023-04-07 09:12:20 -0400

    Error bars

--- Modules/Sim.py
+++ Modules/Sim.py
@@ -26,21 +26,21 @@ p0 = 1500                               # Initial RMM01 and GBM Price
 K2 = 1                                  # Strike of StableVolatility Pool
 P0 = 1                                  # Initial OU and StableVolatility Price
 sigma = np.linspace(0.0001, 0.1, G)     # Pool Implied Volatility Parameter
-T = 0.1                                 # Pool Time Horizon in Years
+T = 1/365                                 # Pool Time Horizon in Years
 dt = 0.015/365                          # Time-Step Size in Years
 N = round(T/dt)                         # Number of Time-Steps
 gamma = 0.9999                          # Fee Regime on CFMM
 shares = 100000                         # Number of Shares in CFMM
 
-v = 0.1             # GBM & OU Volatility Parameter
+v = 0.7             # GBM & OU Volatility Parameter
 mu = 0.0            # GBM Drift Parameter
 
 mean = 1            # OU mean price
-theta = 2/365       # OU mean reversion time
+theta = 0.01        # OU mean reversion time
 
 Arb = 5             # Arbitrage Profit Threshold Denominated in Numeraire
 
-M = 1               # Number of Simulation Runs per IV parameter
+M = 10              # Number of Simulation Runs per IV parameter
 
 # Simulation Processes
 
@@ -97,14 +97,21 @@ def GBMSimProcess(j):
         env.run(until=N)
 
         FeeIncome.append(sum(Fees))
-    return sum(FeeIncome)/M
+    avgIncome = sum(FeeIncome)/M
+    varIncome = sum([(fee - avgIncome)**2 for fee in FeeIncome])/M
+    return avgIncome, varIncome
 
 ## Multithreading
 
-array = []
+avgIncome = []
+varIncome = []
 if run_GBM_simulation:
+    array = []
     with ThreadPoolExecutor() as executor:
         array = list(executor.map(GBMSimProcess, range(G)))
+    avgIncome, varIncome = zip(*array)
+
+stdIncome = [np.sqrt(var) for var in varIncome]
 
 # OU Based Stable Volatility simulation
 
@@ -117,14 +124,21 @@ def OUSimProcess(j):
         env.run(until=N)
 
         FeeIncome.append(sum(Fees))
-    return sum(FeeIncome)/M
+    avgIncome = sum(FeeIncome)/M
+    varIncome = sum([(fee - avgIncome)**2 for fee in FeeIncome])/M
+    return avgIncome, varIncome
 
 ## Multithreading
 
-array2 = []
+avgIncome2 = []
+varIncome2 = []
 if run_OU_simulation:
+    array2 = []
     with ThreadPoolExecutor() as executor:
         array2 = list(executor.map(OUSimProcess, range(G)))
+    avgIncome2, varIncome2 = zip(*array2)
+
+stdIncome2 = [np.sqrt(var) for var in varIncome2]
 
 # Backtest Stable Volatility simulation
 
@@ -158,20 +172,27 @@ if run_ConstantSum_test:
 # Plotting Implied Volatility Parameter vs. Average Fees Generated
 
 if run_GBM_simulation:
-    plt.plot(sigma, array, 'g-')
+    plt.plot(sigma, avgIncome, color='#2BBA58')
+    plt.errorbar(sigma, avgIncome, yerr=stdIncome, ecolor='#BCF2CD', capthick=2)
     plt.title(f"Strike {K1}, Time Horizon = {T} years, Fee = {(1-gamma)*100}%, RV = {v*100}% annualized, Drift = {mu*100}%, GBM RMM-01 Simulation", fontsize=10) 
     plt.xlabel("Implied Volatility Parameter", fontsize=10)
     plt.ylabel("Expected Fees", fontsize=10)
     plt.show()
+    plt.close()
+
 elif run_OU_simulation:
-    plt.plot(sigma, array2, 'g-')
+    plt.plot(sigma, avgIncome2, color='#2BBA58')
+    plt.errorbar(sigma, avgIncome2, yerr=stdIncome2, ecolor='#BCF2CD', capthick=2)
     plt.title(f"Strike {K2}, Time Horizon = {T} years, Fee = {(1-gamma)*100}%, RV = {v*100}% annualized, Mean Price = {mean}, Theta = {theta}, OU Stable Volatility Simulation", fontsize=10)
     plt.xlabel("Implied Volatility Parameter", fontsize=10)
     plt.ylabel("Expected Fees", fontsize=10)
     plt.show()
+    plt.close()
+
 elif run_Backtest:
-    plt.plot(sigma, array3, 'g-')
+    plt.plot(sigma, array3, color='#2BBA58')
     plt.title(f"Strike {K2}, Time Horizon = {T} years, Fee = {(1-gamma)*100}%, RV = 2.56% annualized, Backtest USDC/USDT", fontsize=10)
     plt.xlabel("Implied Volatility Parameter", fontsize=10)
     plt.ylabel("Expected Fees", fontsize=10)
     plt.show()
+    plt.close()
